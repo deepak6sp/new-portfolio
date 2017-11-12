b5068b3a5c118570661e998af6189ec0f6117887
deepak6sp <deepak6sp@gmail.com>
2017-11-12 22:00:41 +1100

    image fixes

--- server.js
+++ server.js
@@ -13,11 +13,13 @@ app.get(['/', '/projects', '/about'], function (req, res) {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-app.get('/projects/*', (req, res, next) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
+app.get('/projects/:projectName', (req, res, next) => {
+    res.redirect("/projects")
+    next
 });
 
 app.use('/images', express.static(path.join(__dirname, 'images')));
 
+app.use('/bundle.js', express.static(path.join(__dirname, 'bundle.js')));
 
 app.listen('8080');
