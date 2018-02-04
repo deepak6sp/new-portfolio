7fdc6d2e5d9453d9dc8fce96024be865b302363b
deepak6sp <deepak6sp@gmail.com>
2018-02-04 14:12:36 +1100

    indecx and server file changes

--- server-prod.js
+++ server-prod.js
@@ -2,13 +2,8 @@ const path = require('path');
 const express = require('express');
 const app = express();
 
-app.get(['/', '/projects', '/about'], function (req, res) {
+app.get(['/', '/projects', '/about', '/projects/:projectName'], function (req, res) {
     res.sendFile(path.join(__dirname, 'index.html'));
-});
-
-app.get('/projects/:projectName', (req, res, next) => {
-    res.redirect("/projects")
-    next
 });
 
 app.use('/images', express.static(path.join(__dirname, 'images')));
